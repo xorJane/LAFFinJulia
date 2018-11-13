f9d5210a4dd2500dd707a8a267d9de8bb3597a0b
xorJane <jherrima@gmail.com>
2018-11-13 11:24:42 -0800

    Add flame library

--- flame.jl
+++ flame.jl
@@ -1,57 +1,373 @@
 module flame
 
-function part_2x1(A::Vector, ATsize=0, side="TOP")
-    if ATsize < 0
-        throw(DimensionMismatch("size < 0"))
-    elseif ATsize > size(A)[1]
-        throw(DimensionMismatch("size > row dimension"))
-    elseif !(side in ("TOP", "BOTTOM"))
-        throw(ArgumentError("""side must be "TOP" or "BOTTOM" """))
+
+"""
+    merge_2x2!(TL::Matrix, TR::Matrix, BL::Matrix, BR::Matrix, A::Matrix)
+
+Modify matrix `A` to contain the values of submatrices/quandrants TL, TR,
+BL, and BR.
+"""
+function merge_2x2!(TL::Matrix, TR::Matrix, BL::Matrix, BR::Matrix, A::Matrix)
+    if size(TL)[1] > 0 && size(TL)[2] > 0
+        for i in 1:size(TL)[1], j in 1:size(TL)[2]
+           A[i, j] = TL[i, j] 
+        end
     end
-    hpart = (side == "TOP") ? ATsize : size(A)[1] - ATsize
-    AT, AB = A[1:hpart], A[hpart + 1:end]
+    
+    if size(TR)[1] > 0 && size(TR)[2] > 0
+        for i in 1:size(TR)[1], j in 1:size(TR)[2]
+           A[i, j + size(TL)[2]] = TR[i, j] 
+        end
+    end
+    
+    if size(BL)[1] > 0 && size(BL)[2] > 0
+        for i in 1:size(BL)[1], j in 1:size(BL)[2]
+           A[i + size(TL)[1], j] = BL[i, j] 
+        end
+    end
+    
+    if size(BR)[1] > 0 && size(BR)[2] > 0
+        for i in 1:size(BR)[1], j in 1:size(BR)[2]
+           A[i + size(TL)[1], j + size(TL)[2]] = BR[i, j] 
+        end
+    end
 end
 
-function repart_2x1_to_3x1(AT::Vector, AB::Vector, m=1, side = "BOTTOM")
-    top_end = (side == "BOTTOM") ? size(AT)[1] : size(AT)[1] - m
-    bottom_start = (side == "TOP") ? 1 : m + 1
+"""
+    merge_2x1!(T::Vector, B::Vector, x::Vector)
 
-    A0 = AT[1:top_end]
-    A2 = AB[bottom_start: end]
-    # What if you're taking from the bottom but the bottom is empty?
-    # Or taking from the top, but the top is empty?
-    # If "BOTTOM", A1 == AB[1:m] if length(AB) >= m,
-    #              A1 == AB[1:0] if length(AB) == 0
-    #              A1 == AB[1:length(AB)] if length(AB) <= m
-    #              Generalization:
-    #              A1 == AB[1:min(length(AB), m)]
-    # What if m > length(AB) > 0?
-    # If "TOP", A1 == AT[end - m + 1: end] if length(AT) >= m,
-    #           A1 == AT[1:end] if length(AT) == 0
-    #           A1 == AT[1:end] if length(AT) <= m
-    #           Generalization:
-    #           A1 == AT[max(1, end - m + 1):end]
-    # What if m > length(AT) > 0?
-    A1 = []
-    if side == "BOTTOM"
-        A1 = AB[1:min(bottom_start - 1, length(AB))]
-    else
-        A1 = AT[max(top_end + 1, 1): end]
+Combine input vectors `T` and `B` to create input/output vector `x`.
+"""
+function merge_2x1!(T::Vector, B::Vector, x::Vector)
+    xtemp = vcat(T, B)
+    for i in 1:length(xtemp)
+        x[i] = xtemp[i]
     end
-    # Expressed another way:
-    # A1 = (side == "BOTTOM") ? AB[1:min(bottom_start - 1, length(AB))] : AT[max(top_end + 1, 1): end]
-    return A0, A1, A2
 end
 
+"""
+    merge_2x1!(T::Matrix, B::Matrix, A::Matrix)
+
+Combine input matrices `T` and `B` vertically to create input/output matrix `A`.
+"""
+function merge_2x1!(T::Matrix, B::Matrix, A::Matrix)
+    Atemp = vcat(T, B)
+    m, n = size(Atemp)
+    for i in 1:m, j in 1:n
+        A[i, j] = Atemp[i, j]
+    end
+end
+
+"""
+    merge_1x2!(L::Matrix, R::Matrix, A::Matrix)
+
+Update matrix `A` to contain the elements of the submatrices
+`L` on the left and `R` on the right.
+"""
+function merge_1x2!(L::Matrix, R::Matrix, A::Matrix)
+    if size(L)[1] > 0 && size(L)[2] > 0
+        for i in 1:size(L)[1], j in 1:size(L)[2]
+            A[i, j] = L[i, j]
+        end
+    end
+    
+    if size(R)[1] > 0 && size(R)[2] > 0
+        for i in 1:size(R)[1], j in 1:size(R)[2]
+           A[i, j + size(L)[2]] = R[i, j] 
+        end
+    end
+end
+
+"""
+    cont_with_1x3_to_1x2(A0::Matrix, A1::Matrix, A2::Matrix, side = "LEFT")
+
+Repartition three submatrices (vertical slabs of an original matrix) into two by combining the middle portion, A1, of the original matrix with the `side` input submatrix.
+"""
+function cont_with_1x3_to_1x2(A0::Matrix, A1::Matrix, A2::Matrix, side = "LEFT")
+    (side == "LEFT") ? (hcat(A0, A1), A2) : (A0, hcat(A1, A2))
+end
+
+"""
+    cont_with_3x1_to_2x1(A0::Vector, A1::Vector, A2::Vector, side = "TOP")
+
+Repartition three subvectors into two by combining the middle portion, A1, of the original vector with the `side` input subvector.
+
+"""
 function cont_with_3x1_to_2x1(A0::Vector, A1::Vector, A2::Vector, side = "TOP")
     (side == "TOP") ? (vcat(A0, A1), A2) : (A0, vcat(A1, A2))
 end
 
-function merge_2x1!(T::Vector, B::Vector, A::Vector)
-    Atemp = vcat(T, B)
-    for i in 1:length(Atemp)
-        A[i] = Atemp[i]
+"""
+    cont_with_3x1_to_2x1(A0::Matrix, A1::Matrix, A2::Matrix, side = "TOP")
+
+Repartition three submatrices (horizontal slabs of an original matrix) into two by combining the middle portion, A1, of the original matrix with the `side` input submatrix.
+"""
+function cont_with_3x1_to_2x1(A0::Matrix, A1::Matrix, A2::Matrix, side = "TOP")
+    (side == "TOP") ? (vcat(A0, A1), A2) : (A0, vcat(A1, A2))
+end
+
+"""
+    cont_with_3x3_to_2x2(A00::Matrix, A01::Matrix, A02::Matrix,
+                         A10::Matrix, A11::Matrix, A12::Matrix,
+                         A20::Matrix, A21::Matrix, A22::Matrix, quad = "TL")  
+
+Concatenate submatrices together to repartition the original matrix into 4 quadrants
+rather than a 3x3 grid. The middle submatrix A11 is included in the `quad` output quadrant.
+"""
+function cont_with_3x3_to_2x2(A00::Matrix, A01::Matrix, A02::Matrix,
+                              A10::Matrix, A11::Matrix, A12::Matrix,
+                              A20::Matrix, A21::Matrix, A22::Matrix, quad = "TL")
+    if quad == "TL"
+        TL = hcat(vcat(A00, A10), vcat(A01, A11))
+        TR = vcat(A02, A12)
+        BL = hcat(A20, A21)
+        BR = A22
+    elseif quad == "TR"
+        TL = vcat(A00, A10)
+        TR = hcat(vcat(A01, A11), vcat(A02, A12))
+        BL = A20
+        BR = hcat(A21, A22)
+    elseif quad == "BL"
+        TL = hcat(A00, A01)
+        TR = A02
+        BL = hcat(vcat(A10, A20), vcat(A11, A21))
+        BR = vcat(A12, A22)
+    elseif quad == "BR"
+        TL = A00
+        TR = hcat(A01, A02)
+        BL = vcat(A10, A20)
+        BR = hcat(vcat(A11, A21), vcat(A12, A22))       
     end
+    
+    return TL, TR, BL, BR
+end
+
+"""
+    part_1x2(A::Matrix, vpart = 0, side = "LEFT")
+
+Partition a matrix into left and righthand portions, with
+`vpart` columns in the `side`hand side.
+"""
+function part_1x2(A::Matrix, vpart = 0, side = "LEFT")
+    if vpart < 0
+        throw(DimensionMismatch("size < 0"))
+    elseif vpart > size(A)[2]
+        throw(DimensionMismatch("size > col dimension"))
+    elseif !(side in ("LEFT", "RIGHT"))
+        throw(ArgumentError("""side must be "LEFT" or "RIGHT" """))
+    end
+    
+    vpart = (side == "LEFT") ? vpart : size(A)[2] - vpart
+    
+    AL, AR = A[:, 1:vpart], A[:, vpart + 1:end]
+end
+
+"""
+    part_2x1(x::Vector, hpart=0, side="TOP")
+
+Partition a vector into top and bottom portions, with
+`hpart` elements in the `side` portion.
+"""
+function part_2x1(x::Vector, hpart=0, side="TOP")
+    if hpart < 0
+        throw(DimensionMismatch("size < 0"))
+    elseif hpart > size(x)[1]
+        throw(DimensionMismatch("size > row dimension"))
+    elseif !(side in ("TOP", "BOTTOM"))
+        throw(ArgumentError("""side must be "TOP" or "BOTTOM" """))
+    end
+    hpart = (side == "TOP") ? hpart : size(x)[1] - hpart
+    xT, xB = x[1:hpart], x[hpart + 1:end]
+end
+
+"""
+    part_2x1(A::Matrix, hpart = 0, side = "TOP")
+
+Partition a matrix into top and bottom portions, with
+`hpart` rows in the `side` portion.
+"""
+function part_2x1(A::Matrix, hpart=0, side="TOP")
+    if hpart < 0
+        throw(DimensionMismatch("size < 0"))
+    elseif hpart > size(A)[1]
+        throw(DimensionMismatch("size > row dimension"))
+    elseif !(side in ("TOP", "BOTTOM"))
+        throw(ArgumentError("""side must be "TOP" or "BOTTOM" """))
+    end
+    hpart = (side == "TOP") ? hpart : size(A)[1] - hpart
+    AT, AB = A[1:hpart, :], A[hpart + 1:end, :]
+end
+
+"""
+    part_2x2(A::Matrix, m::Int, n::Int, quad::String)
+
+Break `A` into four quadrants. `quad` specifies the quadrant
+that should be `m` x `n`.
+"""
+function part_2x2(A::Matrix, m::Int, n::Int, quad::String)
+    if !(quad in ("TL", "TR", "BL", "BR"))
+        throw(ArgumentError("""quad must be "TL", "TR", "BL", or "BR"."""))
+    end
+    
+    hpart = (quad in ("TL", "TR")) ? m : size(A)[1] - m
+    vpart = (quad in ("TL", "BL")) ? n : size(A)[2] - n
+    
+    TL, TR = A[1:hpart, 1:vpart], A[1:hpart, (vpart + 1):end]
+    BL, BR = A[(hpart + 1):end, 1:vpart], A[(hpart + 1):end, (vpart + 1):end]
+    
+    return TL, TR, BL, BR
+end
+
+"""
+    repart_1x2_to_1x3(AL::Matrix, AR::Matrix, n=1, side = "RIGHT")
+
+Takes two submatrices and breaks the `side`hand portion into two
+submatrices. `n` specifies the number of columns in what is ultimately
+the middle submatrix.
+"""
+function repart_1x2_to_1x3(AL::Matrix, AR::Matrix, n=1, side = "RIGHT")
+    if side == "RIGHT"
+        vpart = n
+        A0 = AL
+        A1 = AR[:, 1:vpart]
+        A2 = AR[:, (vpart + 1):end]
+    else
+        vpart = size(AL)[2] - n
+        A0 = AL[:, 1:vpart]
+        A1 = AL[:, (vpart + 1):end]
+        A2 = AR
+    end
+    
+    return A0, A1, A2
+end
+
+"""
+    repart_2x1_to_3x1(AT::Vector, AB::Vector, m=1, side = "BOTTOM")
+
+Repartition a vector already divided into top and bottom portions,
+`AT` and `AB`, into three horizontal slabs, `A0`, `A1`, and `A2`.
+The middle segment, `A1`, is created from the `m` elements in the `side`
+input portion that are closest to the interface between `AT` and `AB`.
+
+For example, if side == "BOTTOM", A0 == AT; A1 is created from the top m
+elements from AB, and A2 is what remains of AB after its m elements are excluded.
+"""
+function repart_2x1_to_3x1(xT::Vector, xB::Vector, m=1, side = "BOTTOM")
+    top_end = (side == "BOTTOM") ? size(xT)[1] : size(xT)[1] - m
+    bottom_start = (side == "TOP") ? 1 : m + 1
+
+    x0 = xT[1:top_end]
+    x2 = xB[bottom_start: end]
+    # What if you're taking from the bottom but the bottom is empty?
+    # Or taking from the top, but the top is empty?
+    # If "BOTTOM", x1 == xB[1:m] if length(xB) >= m,
+    #              x1 == xB[1:0] if length(xB) == 0
+    #              x1 == xB[1:length(xB)] if length(xB) <= m
+    #              Generalization:
+    #              x1 == xB[1:min(length(xB), m)]
+    # What if m > length(xB) > 0?
+    # If "TOP", x1 == xT[end - m + 1: end] if length(xT) >= m,
+    #           x1 == xT[1:end] if length(xT) == 0
+    #           x1 == xT[1:end] if length(xT) <= m
+    #           Generalization:
+    #           x1 == xT[max(1, end - m + 1):end]
+    # What if m > length(xT) > 0?
+    x1 = []
+    if side == "BOTTOM"
+        x1 = xB[1:min(bottom_start - 1, length(xB))]
+    else
+        x1 = xT[max(top_end + 1, 1): end]
+    end
+    # Expressed another way:
+    # x1 = (side == "BOTTOM") ? xB[1:min(bottom_start - 1, length(xB))] : xT[max(top_end + 1, 1): end]
+    return x0, x1, x2
+end
+
+"""
+    repart_2x1_to_3x1(AT::Matrix, AB::Matrix, m=1, side = "BOTTOM")
+
+Repartition a matrix already divided into top and bottom portions,
+`AT` and `AB`, into three horizontal slabs, `A0`, `A1`, and `A2`.
+The middle segment, `A1`, is created from the `m` rows in the `side`
+input portion that are closest to the interface between `AT` and `AB`.
+
+For example, if side == "BOTTOM", A0 == AT; A1 is created from the top m
+rows from AB, and A2 is what remains of AB after its m rows are excluded.
+"""
+function repart_2x1_to_3x1(AT::Matrix, AB::Matrix, m=1, side = "BOTTOM")
+    top_end = (side == "BOTTOM") ? size(AT)[1] : size(AT)[1] - m
+    bottom_start = (side == "TOP") ? 1 : m + 1
+
+    A0 = AT[1:top_end, :]
+    A2 = AB[bottom_start: end, :]
+    A1 = []
+    if side == "BOTTOM"
+        A1 = AB[1:min(bottom_start - 1, length(AB)), :]
+    else
+        A1 = AT[max(top_end + 1, 1): end, :]
+    end
+    # Expressed another way:
+    # A1 = (side == "BOTTOM") ? AB[1:min(bottom_start - 1, length(AB)), :] : AT[max(top_end + 1, 1): end, :]
+    return A0, A1, A2
+end
+
+"""
+    repart_2x2_to_3x3(ATL::Matrix, ATR::Matrix,
+                           ABL::Matrix, ABR::Matrix, m = 1, n = 1, quad = "BR")
+
+Repartition a 2x2 matrix into a 3x3 matrix. The `quad` quadrant is broken into four
+submatrices and the interior matrix returned, `A22`, will be m x n.
+"""
+function repart_2x2_to_3x3(ATL::Matrix, ATR::Matrix,
+                           ABL::Matrix, ABR::Matrix, m = 1, n = 1, quad = "BR")
+    hpart = (quad in ("TL", "TR")) ? size(ATL, 1) - m : m
+    vpart = (quad in ("TL", "BL")) ? size(ATL, 2) - n : n
+    
+    if quad == "TL"
+        A11, A12, A13 = ATL[1:hpart, 1:vpart], ATL[1:hpart, (vpart + 1):end], ATR[1:hpart, :]
+        A21, A22, A23 = ATL[(hpart + 1):end, 1:vpart], ATL[(hpart + 1):end, (vpart + 1):end], ATR[(hpart + 1):end, :]
+        A31, A32, A33 = ABL[:, 1:vpart], ABL[:, (vpart + 1):end], ABR[:, :]
+    elseif quad == "TR"
+        A11, A12, A13 = ATL[1:hpart, :], ATR[1:hpart, 1:vpart], ATR[1:hpart, (vpart + 1):end]
+        A21, A22, A23 = ATL[(hpart + 1):end, :], ATR[(hpart + 1):end, 1:vpart], ATR[(hpart + 1):end, (vpart + 1):end]
+        A31, A32, A33 = ABL[:, :], ABR[:, 1:vpart], ABR[:, (vpart + 1):end]
+    elseif quad == "BL"
+        A11, A12, A13 = ATL[:, 1:vpart], ATL[:, (vpart + 1):end], ATR[:, :]
+        A21, A22, A23 = ABL[1:hpart, 1:vpart], ABL[1:hpart, (vpart + 1):end], ABR[1:hpart, :]
+        A31, A32, A33 = ABL[(hpart + 1):end, 1:vpart], ABL[(hpart + 1):end, (vpart + 1):end], ABR[(hpart + 1):end, :]
+    elseif quad == "BR"
+        A11, A12, A13 = ATL[:, :], ATR[:, 1:vpart], ATR[:, (vpart + 1):end]
+        A21, A22, A23 = ABL[1:hpart, :], ABR[1:hpart, 1:vpart], ABR[1:hpart, (vpart + 1):end]
+        A31, A32, A33 = ABL[(hpart + 1):end, :], ABR[(hpart + 1):end, 1:vpart], ABR[(hpart + 1):end, (vpart + 1):end]
+    end
+    
+    return ( A11, A12, A13,
+             A21, A22, A23,
+             A31, A32, A33 )
+    
 end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test_flame.jl
+++ test_flame.jl
@@ -2,55 +2,263 @@ include("./flame.jl")
 using .flame
 using Test
 
-@testset "part_2x1 for Vector" begin
-    x = [1, 2, 3, 4]
-    @test_throws DimensionMismatch flame.part_2x1(x, -1)
-    @test_throws DimensionMismatch flame.part_2x1(x, 5)
-    @test_throws ArgumentError flame.part_2x1(x, 0, "HELLO")
-    @test flame.part_2x1(x, 0, "TOP") == ([], x)
-    @test flame.part_2x1(x, 0, "BOTTOM") == (x, [])
-    @test flame.part_2x1(x, 1, "TOP") == (x[1:1], x[2:end])
-    @test flame.part_2x1(x, 1, "BOTTOM") == (x[1:end - 1], x[end:end])
-    @test flame.part_2x1(x, 2, "TOP") == (x[1:2], x[3:end])
-    @test flame.part_2x1(x, 2, "BOTTOM") == (x[1:end - 2], x[end - 1:end])
-    @test flame.part_2x1(x, 4, "TOP") == (x[1:end], [])
-    @test flame.part_2x1(x, 4, "BOTTOM") == ([], x[1:end])
-end
-
-@testset "repart_2x1repart_2x1_to_3x1 for Vector" begin
-    x = [1, 2, 3, 4]
-    for ATsize in 0:length(x)
-        AT = x[1:ATsize]
-        AB = x[ATsize + 1:end]
-
-        m = 1
-        # For "bottom"
-        A0 = AT[1:ATsize]
-        A1 = AB[1:min(m, length(AB))]
-        A2 = AB[m + 1:end]
-        @test flame.repart_2x1_to_3x1(AT, AB, m, "BOTTOM") == (A0, A1, A2)
-        # For "top"
-        A0 = AT[1:ATsize - m]
-        A1 = AT[max(ATsize - m + 1, 1):end]
-        A2 = AB[1:end]
-        @test flame.repart_2x1_to_3x1(AT, AB, m, "TOP") == (A0, A1, A2)
+@testset "merge_2x2" begin
+    # Divide square matrix into four evenly
+    # sized quadrants and reconstruct
+    A = reshape([x for x in 1:16], 4, 4)
+    Anew = fill(0, 4, 4)
+    TL = A[1:2, 1:2]
+    TR = A[1:2, 3:4]
+    BL = A[3:4, 1:2]
+    BR = A[3:4, 3:4]
+    flame.merge_2x2!(TL, TR, BL, BR, Anew) 
+    @test Anew == A
+    ## Automate testing for matrices of various
+    #  sizes
+    for m in 2:5, n in 2:5
+        A = reshape([x for x in 1:m*n], m, n)
+        Anew = fill(0, m, n)
+        # pick horizontal and vertical lines
+        # along which to divide A into quadrants.
+        for hsplit in 1:(m - 1), vsplit in 1:(n - 1)
+            TL = A[1:hsplit, 1:vsplit]
+            TR = A[1:hsplit, (vsplit + 1):end]
+            BL = A[(hsplit + 1):end, 1:vsplit]
+            BR = A[(hsplit + 1):end, (vsplit + 1):end]
+            flame.merge_2x2!(TL, TR, BL, BR, Anew) 
+            @test Anew == A
+        end
     end
 end
 
-@testset "cont_with_3x1_to_2x1 for Vectors" begin
-    A0 = [1, 2, 3]
-    A1 = [4]
-    A2 = [5]
-    @test flame.cont_with_3x1_to_2x1(A0, A1, A2) == ([1, 2, 3, 4], [5])
-    @test flame.cont_with_3x1_to_2x1(A0, A1, A2, "BOTTOM") == ([1, 2, 3], [4, 5])
+@testset "merge_2x1" begin
+    @testset "for Vectors" begin
+        yT = [1, 2, 3]
+        yB = [4, 5]
+        y = fill(0, 5)
+        flame.merge_2x1!(yT, yB, y)
+        @test y == [1, 2, 3, 4, 5]
+    end
+
+    @testset "for Matrices" begin
+        AT = [1 2 3;]
+        AB = [4 5 6; 7 8 9]
+        A = fill(0, 3, 3)
+        flame.merge_2x1!(AT, AB, A)
+        @test A == [i + 3*(j - 1) for j in 1:3, i in 1:3]
+    end
 end
 
-@testset "merge_2x1 for Vectors" begin
-    yT = [1, 2, 3]
-    yB = [4, 5]
-    y = fill(0, 5)
-    flame.merge_2x1!(yT, yB, y)
-    @test y == [1, 2, 3, 4, 5]
-
-
+@testset "merge_1x2!" begin
+    A = reshape([x for x in 1:25], 5, 5)
+    Anew = fill(0, 5, 5)
+    L = A[:, 1:2]
+    R = A[:, 3:5]
+    flame.merge_1x2!(L, R, Anew)
+    @test A == Anew
+    Anew = fill(0, 1, 5)
+    L = A[1:1, 1:2]
+    R = A[1:1, 3:5]
+    flame.merge_1x2!(L, R, Anew)
+    @test A[1:1, :] == Anew
 end
+
+@testset "cont_with_1x3_to_1x2" begin
+    @testset "for Matrices" begin
+        m, n = 3, 4
+        A = [(j + n*(i - 1)) for i in 1:m, j in 1:n]
+        A0 = A[:, 1:2]
+        A1 = A[:, 3:3]
+        A2 = A[:, 4:end]
+        @test flame.cont_with_1x3_to_1x2(A0, A1, A2) == ([1 2 3; 5 6 7; 9 10 11], A2)
+        @test flame.cont_with_1x3_to_1x2(A0, A1, A2, "RIGHT") == (A0, [3 4; 7 8; 11 12])
+    end
+end
+
+@testset "cont_with_3x1_to_2x1" begin
+    @testset "for Vectors" begin
+        A0 = [1, 2, 3]
+        A1 = [4]
+        A2 = [5]
+        @test flame.cont_with_3x1_to_2x1(A0, A1, A2) == ([1, 2, 3, 4], [5])
+        @test flame.cont_with_3x1_to_2x1(A0, A1, A2, "BOTTOM") == ([1, 2, 3], [4, 5])
+    end
+    @testset "for Matrices" begin
+        A0 = [1 2 3;]
+        A1 = [4 5 6;]
+        A2 = [7 8 9;]
+        @test flame.cont_with_3x1_to_2x1(A0, A1, A2) == ([1 2 3; 4 5 6], A2)
+        @test flame.cont_with_3x1_to_2x1(A0, A1, A2, "BOTTOM") == (A0, [4 5 6; 7 8 9])
+    end
+end
+
+@testset "cont_with_3x3_to_2x2" begin
+    A = [1 2 3; 4 5 6; 7 8 9]
+    A00, A01, A02 = A[1:1, 1:1], A[1:1, 2:2], A[1:1, 3:3]
+    A10, A11, A12 = A[2:2, 1:1], A[2:2, 2:2], A[2:2, 3:3]
+    A20, A21, A22 = A[3:3, 1:1], A[3:3, 2:2], A[3:3, 3:3]
+    # Test each configuration of recombining
+    TL, TR, BL, BR = flame.cont_with_3x3_to_2x2(A00, A01, A02, A10, A11, A12, A20, A21, A22,"TL")
+    @test (TL, TR, BL, BR) == (A[1:2, 1:2], A[1:2, 3:3], A[3:3, 1:2], A[3:3, 3:3])
+    TL, TR, BL, BR = flame.cont_with_3x3_to_2x2(A00, A01, A02, A10, A11, A12, A20, A21, A22,"TR")
+    @test (TL, TR, BL, BR) == (A[1:2, 1:1], A[1:2, 2:3], A[3:3, 1:1], A[3:3, 2:3]) 
+    TL, TR, BL, BR = flame.cont_with_3x3_to_2x2(A00, A01, A02, A10, A11, A12, A20, A21, A22,"BL")
+    @test (TL, TR, BL, BR) == (A[1:1, 1:2], A[1:1, 3:3], A[2:3, 1:2], A[2:3, 3:3])
+    TL, TR, BL, BR = flame.cont_with_3x3_to_2x2(A00, A01, A02, A10, A11, A12, A20, A21, A22,"BR")
+    @test (TL, TR, BL, BR) == (A[1:1, 1:1], A[1:1, 2:3], A[2:3, 1:1], A[2:3, 2:3])
+    
+end
+
+@testset "part 1x2 for Matrix" begin
+    A = [1 2 3; 4 5 6; 7 8 9]
+    @test_throws DimensionMismatch flame.part_1x2(A, -1)
+    @test_throws DimensionMismatch flame.part_1x2(A, size(A)[2] + 1)
+    @test_throws ArgumentError flame.part_1x2(A, 0, "HELLO")
+    @test flame.part_1x2(A, 0, "LEFT") == (Array{Int64}(undef, 3, 0), A)
+    @test flame.part_1x2(A, 0, "RIGHT") == (A, Array{Int64}(undef, 3, 0))
+    @test flame.part_1x2(A, 1, "LEFT") == (reshape(A[:, 1], 3, 1), A[:, 2:3])
+    @test flame.part_1x2(A, 1, "RIGHT") == (A[:, 1:2], reshape(A[:, 3], 3, 1))
+    @test flame.part_1x2(A, 3, "LEFT") == (A, Array{Int64}(undef, 3, 0))
+    @test flame.part_1x2(A, 3, "RIGHT") == (Array{Int64}(undef, 3, 0), A )
+end
+
+@testset "part_2x1" begin
+    @testset "for Vectors" begin
+        x = [1, 2, 3, 4]
+        @test_throws DimensionMismatch flame.part_2x1(x, -1)
+        @test_throws DimensionMismatch flame.part_2x1(x, 5)
+        @test_throws ArgumentError flame.part_2x1(x, 0, "HELLO")
+        @test flame.part_2x1(x, 0, "TOP") == ([], x)
+        @test flame.part_2x1(x, 0, "BOTTOM") == (x, [])
+        @test flame.part_2x1(x, 1, "TOP") == (x[1:1], x[2:end])
+        @test flame.part_2x1(x, 1, "BOTTOM") == (x[1:end - 1], x[end:end])
+        @test flame.part_2x1(x, 2, "TOP") == (x[1:2], x[3:end])
+        @test flame.part_2x1(x, 2, "BOTTOM") == (x[1:end - 2], x[end - 1:end])
+        @test flame.part_2x1(x, 4, "TOP") == (x[1:end], [])
+        @test flame.part_2x1(x, 4, "BOTTOM") == ([], x[1:end])
+    end
+    @testset "for Matrices" begin
+        A = reshape([x for x in 1:16], 4, 4)
+        @test_throws DimensionMismatch flame.part_2x1(A, -1)
+        @test_throws DimensionMismatch flame.part_2x1(A, 5)
+        @test_throws ArgumentError flame.part_2x1(A, 0, "HELLO")
+        @test flame.part_2x1(A, 0, "TOP") == (Array{Int64}(undef, 0, 4), A)
+        @test flame.part_2x1(A, 0, "BOTTOM") == (A, Array{Int64}(undef, 0, 4),)
+        @test flame.part_2x1(A, 1, "TOP") == (A[1:1, :], A[2:end, :])
+        @test flame.part_2x1(A, 1, "BOTTOM") == (A[1:end - 1, :], A[end:end, :])
+        @test flame.part_2x1(A, 2, "TOP") == (A[1:2, :], A[3:end, :])
+        @test flame.part_2x1(A, 2, "BOTTOM") == (A[1:end - 2, :], A[end - 1:end, :])
+        @test flame.part_2x1(A, 4, "TOP") == (A[1:end, :], Array{Int64}(undef, 0, 4))
+        @test flame.part_2x1(A, 4, "BOTTOM") == (Array{Int64}(undef, 0, 4), A[1:end, :])
+    end
+end
+
+@testset "part_2x2" begin
+    m, n = 5,5
+    A = reshape([x for x in 1:m*n], m, n)
+    for m in 0:(m - 1), n in 0:(n - 1)
+        TL, TR, BL, BR = flame.part_2x2(A, m, n, "TL")
+        @test size(TL) == (m, n)
+        @test size(BR) == size(A) .- (m, n)
+        @test size(TR) == (m, size(A)[2] - n)
+        @test size(BL) == (size(A)[1] - m, n)
+    end
+    
+    for m in 0:(m - 1), n in 0:(n - 1)
+        TL, TR, BL, BR = flame.part_2x2(A, m, n, "TR")
+        @test size(TL) == (m, size(A)[2] - n)
+        @test size(BR) == (size(A)[1] - m, n)
+        @test size(TR) == (m, n)
+        @test size(BL) == size(A) .- (m, n)
+    end
+    
+    for m in 0:(m - 1), n in 0:(n - 1)
+        TL, TR, BL, BR = flame.part_2x2(A, m, n, "BL")
+        @test size(TL) == (size(A)[1] - m, n)
+        @test size(BR) == (m, size(A)[2] - n)
+        @test size(TR) == size(A) .- (m, n)
+        @test size(BL) == (m, n)
+    end
+    
+    for m in 0:(m - 1), n in 0:(n - 1)
+        TL, TR, BL, BR = flame.part_2x2(A, m, n, "BR")
+        @test size(TL) == size(A) .- (m, n)
+        @test size(BR) == (m, n)
+        @test size(TR) == (size(A)[1] - m, n)
+        @test size(BL) == (m, size(A)[2] - n)
+    end
+end
+
+@testset "repart_1x2_to_1x3" begin
+    A = [1 2 3; 4 5 6; 7 8 9]
+    n = 1
+    @test flame.repart_1x2_to_1x3(A[:, 1:1], A[:, 2:3], n, "RIGHT") == (A[:, 1:1], A[:, 2:2], A[:, 3:3])
+    @test flame.repart_1x2_to_1x3(A[:, 1:2], A[:, 3:3], n, "LEFT") == (A[:, 1:1], A[:, 2:2], A[:, 3:3])
+    # What if one of the portions of A is too small to further subdivide?
+    @test flame.repart_1x2_to_1x3(A[:, 1:1], A[:, 2:3], n, "LEFT") == (Array{Int64}(undef, 3, 0), A[:, 1:1], A[:, 2:3])
+    @test flame.repart_1x2_to_1x3(A[:, 1:2], A[:, 3:3], n, "RIGHT") == (A[:, 1:2], A[:, 3:3], Array{Int64}(undef, 3, 0))
+end
+
+@testset "repart_2x1_to_3x1" begin
+    @testset "for Vectors" begin
+        x = [1, 2, 3, 4]
+        for ATsize in 0:length(x)
+            AT = x[1:ATsize]
+            AB = x[ATsize + 1:end]
+
+            m = 1
+            # For "bottom"
+            A0 = AT[1:ATsize]
+            A1 = AB[1:min(m, length(AB))]
+            A2 = AB[m + 1:end]
+            @test flame.repart_2x1_to_3x1(AT, AB, m, "BOTTOM") == (A0, A1, A2)
+            # For "top"
+            A0 = AT[1:ATsize - m]
+            A1 = AT[max(ATsize - m + 1, 1):end]
+            A2 = AB[1:end]
+            @test flame.repart_2x1_to_3x1(AT, AB, m, "TOP") == (A0, A1, A2)
+        end
+    end
+    @testset "for Matrices" begin
+        A = reshape([i for i in 1:16], 4, 4)
+        for ATsize in 0:size(A)[1]
+            AT = A[1:ATsize, :]
+            AB = A[ATsize + 1:end, :]
+
+            m = 1
+            # For "bottom"
+            A0 = AT[1:ATsize, :]
+            A1 = AB[1:min(m, length(AB)), :]
+            A2 = AB[m + 1:end, :]
+            @test flame.repart_2x1_to_3x1(AT, AB, m, "BOTTOM") == (A0, A1, A2)
+            # For "top"
+            A0 = AT[1:ATsize - m, :]
+            A1 = AT[max(ATsize - m + 1, 1):end, :]
+            A2 = AB[1:end, :]
+            @test flame.repart_2x1_to_3x1(AT, AB, m, "TOP") == (A0, A1, A2)
+        end
+    end
+end
+
+@testset "repart_2x2_to_3x3" begin
+    A = reshape([i for i in 1:16], 4, 4)
+    ATL, ATR = A[1:2, 1:2], A[1:2, 3:4]
+    ABL, ABR = A[3:4, 1:2], A[3:4, 3:4]
+    output_matrices = flame.repart_2x2_to_3x3(ATL, ATR, ABL, ABR, 1, 1, "TL")
+    @test output_matrices[1:3] == (ATL[1:1, 1:1], ATL[1:1, 2:2], ATR[1:1, :])
+    @test output_matrices[4:6] == (ATL[2:2, 1:1], ATL[2:2, 2:2], ATR[2:2, :])
+    @test output_matrices[7:9] == (ABL[:, 1:1], ABL[:, 2:2], ABR[:, :])
+    output_matrices = flame.repart_2x2_to_3x3(ATL, ATR, ABL, ABR, 1, 1, "TR")
+    @test output_matrices[1:3] == (ATL[1:1, :], ATR[1:1, 1:1], ATR[1:1, 2:2])
+    @test output_matrices[4:6] == (ATL[2:2, :], ATR[2:2, 1:1], ATR[2:2, 2:2])
+    @test output_matrices[7:9] == (ABL[:, :], ABR[:, 1:1], ABR[:, 2:2])
+    output_matrices = flame.repart_2x2_to_3x3(ATL, ATR, ABL, ABR, 1, 1, "BL")
+    @test output_matrices[1:3] == (ATL[:, 1:1], ATL[:, 2:2], ATR[:, :])
+    @test output_matrices[4:6] == (ABL[1:1, 1:1], ABL[1:1, 2:2], ABR[1:1, :])
+    @test output_matrices[7:9] == (ABL[2:2, 1:1], ABL[2:2, 2:2], ABR[2:2, :])
+    output_matrices = flame.repart_2x2_to_3x3(ATL, ATR, ABL, ABR, 1, 1, "BR")
+    @test output_matrices[1:3] == (ATL[:, :], ATR[:, 1:1], ATR[:, 2:2])
+    @test output_matrices[4:6] == (ABL[1:1, :], ABR[1:1, 1:1], ABR[1:1, 2:2])
+    @test output_matrices[7:9] == (ABL[2:2, :], ABR[2:2, 1:1], ABR[2:2, 2:2])
+end
+
