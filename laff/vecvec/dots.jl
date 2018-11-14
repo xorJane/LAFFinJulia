447875776e0213bb97163fe6f0b08c34672fe5dc
xorJane <jherrima@gmail.com>
2018-11-14 13:19:24 -0800

    Add some week 4 notebooks

--- laff/vecvec/dots.jl
+++ laff/vecvec/dots.jl
@@ -5,7 +5,7 @@ Compute alpha = x' * y + alpha, storing the result in alpha and updating alpha.
 
 x and y can be `Array`s or transposed `Array`s. For example, x and y can be column vectors (`Vector`s) or row vectors (transposed `Vector`s). If necessary, an implicit transposition happens. alpha is an `Array` containing a single number.
 """
-function dot!(x::Union{LinearAlgebra.Transpose{T, Array{T}} where T <: Number, Array{T} where T <:Number}, y::Union{LinearAlgebra.Transpose{T, Array{T}} where T <: Number, Array{T} where T <:Number}, alpha::Array{T} where T <: Number)
+function dots!(x::Union{LinearAlgebra.Transpose{T, Array{T}} where T <: Number, Array{T} where T <:Number}, y::Union{LinearAlgebra.Transpose{T, Array{T}} where T <: Number, Array{T} where T <:Number}, alpha::Array{T} where T <: Number)
 
     @assert length(alpha) == 1 "laff.dot!: alpha must contain one element."
 
