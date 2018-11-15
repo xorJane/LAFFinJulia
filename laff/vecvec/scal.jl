49ced023e1cb4afbb435b7dd948798696ef67a29
xorJane <jherrima@gmail.com>
2018-11-15 14:31:45 -0800

    Make laff.scal(bang) work with alpha as a one element numeric matrix, not just as a number

--- laff/vecvec/scal.jl
+++ laff/vecvec/scal.jl
@@ -11,3 +11,18 @@ function scal!(alpha::Number, x::Union{Array{T} where T <:Number, LinearAlgebra.
         x[i] = alpha * x[i]
     end
 end
+
+"""
+    scal!(alpha, x)
+
+Compute alpha * x, overwriting x.
+
+alpha must be a `Matrix` with one numeric element. x can be an `Array` or the transpose of an Array.
+"""
+function scal!(alpha::Matrix{T} where T <:Number, x::Union{Array{T} where T <:Number, LinearAlgebra.Transpose{T, Array{T}} where T <: Number})
+    @assert length(alpha) == 1 "laff.scal!: alpha should be a Number or a Matrix with one numeric element!"
+    m = length(x)
+    for i in 1:m
+        x[i] = alpha[1] * x[i]
+    end
+end
